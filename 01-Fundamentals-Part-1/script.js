f0dbf29306db9cf4c4cd7a25399fdd43cbec0ed9
Karem <karemdarwesh1035@gmail.com>
2023-12-27 02:15:54 +0200

    // LECTURE: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,3 +4,16 @@ let population = '130 Millions';
 console.log(myCountry);
 console.log(continent);
 console.log(population);
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language);
