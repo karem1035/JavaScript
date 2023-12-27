5f46ad2918165b5ef7f3e266a7f623dc902e606b
Karem <karemdarwesh1035@gmail.com>
2023-12-27 02:50:35 +0200

    LECTURE: let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -3,9 +3,9 @@
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-let myCountry = 'Egypt';
-let continent = 'Africa';
-let population = '130 Millions';
+const myCountry = 'Egypt';
+const continent = 'Africa';
+const population = '130 Millions';
 console.log(myCountry);
 console.log(continent);
 console.log(population);
@@ -17,8 +17,21 @@ console.log(population);
 // 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 // to the console
 
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(isIsland);
+console.log(language);
+
+/*
+LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = 'English';
+language = 'Arabic';
 console.log(language);
---
+++ notes.md
@@ -0,0 +1,1 @@
+# Note While watching the course
