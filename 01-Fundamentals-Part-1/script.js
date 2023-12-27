5d888304a6e6fb264b4d6e08e30cf3ea0af22eed
Karem <karemdarwesh1035@gmail.com>
2023-12-27 02:16:26 +0200

    semi update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,8 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
 let myCountry = 'Egypt';
 let continent = 'Africa';
 let population = '130 Millions';
